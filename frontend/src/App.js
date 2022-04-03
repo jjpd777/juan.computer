afb2139ad9310344ad83b6e6a57a4b48ec24e62d
juan <jpalacio@axletravel.com>
2022-04-02 21:14:40 -0600

    Added metamask login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MetaMaskAuth from "./MetamaskLogin";
 
+
+const mm = "https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="logoContainer">
+        <img src={mm} height={90}/>
+      </div>
+      <div>
+        <p>Do you want to find out?</p>
+      </div>
+      <MetaMaskAuth onAddressChanged={address => {}}/>
+    </main>
   );
 }
 
